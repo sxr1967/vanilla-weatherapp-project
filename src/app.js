4cce761c47ca794e834d7ffbb85821a106c40d17
Snehal Ingle <sxr1967@g.rit.edu>
2020-08-19 23:07:24 -0400

    added .js + api
    Added .js scripts + api to give real-time values to placeholders from the draft
    date, temp, humidity, wind-speed etc.
    
    Next: Add a search engine to look up new city (search button) + Add geolocation (current button)

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
+function weatherDisplay(response) {
+  console.log(response);
+  console.log(response.data.main.temp);
+  console.log(response.data.name);
+  console.log(response.data.weather[0].description);
+  console.log(response.data.wind.speed);
+  console.log(response.data.main.humidity);
 
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let humidityElement = document.querySelector("#humidity");
+  let weatherDescriptionElement = document.querySelector("#weatherDescription");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("display-time");
+
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  humidityElement.innerHTML = response.data.main.humidity;
+  weatherDescriptionElement.innerHTML = response.data.weather[0].description;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiurl =
+  "https://api.openweathermap.org/data/2.5/weather?q=New York&appid=5fc91e4f79dee96c1ed3234e310a83cf&units=metric";
+
+console.log(apiurl);
+axios.get(apiurl).then(weatherDisplay);
